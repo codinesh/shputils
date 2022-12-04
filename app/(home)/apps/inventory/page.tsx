51b8ce6d2e392203bbf4627aa6860bfe0bf937ca
Dinesh <7272358+codinesh@users.noreply.github.com>
2022-12-04 23:17:12 +0530

    Update page.tsx

--- app/(home)/apps/inventory/page.tsx
+++ app/(home)/apps/inventory/page.tsx
@@ -60,7 +60,7 @@ const Page = () => {
                   <th
                     scope='col'
                     className='text-sm font-medium text-gray-900 px-6 py-4 text-left'>
-                    Assigned on
+                    Added on
                   </th>
                 </tr>
               </thead>
